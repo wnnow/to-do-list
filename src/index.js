bc3dc5ea41725c0b5bc0a1a41f721bf65439377b
wnnow <117201813+wnnow@users.noreply.github.com>
2024-02-08 16:59:00 +0700

    Add footerLogo for webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import "./style.css";
 import { renderProjectNavbar } from "./modules/project.js";
 import { addEventListenerCloseTaskDetailBtn } from "./modules/taskDetailPopup.js";
+import GitHubLogo from "./images/github-mark.svg";
 renderProjectNavbar();
 addEventListenerCloseTaskDetailBtn();
+function addFooterLogo() {
+  const footerLink = document.querySelector(".footer-link");
+  const logoImg = document.createElement("img");
+  const gitHubLogo = new Image();
+  gitHubLogo.src = GitHubLogo;
+  gitHubLogo.classList.add("footer-github-logo");
+  footerLink.appendChild(gitHubLogo);
+}
+addFooterLogo();
